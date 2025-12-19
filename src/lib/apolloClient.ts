1a2ebe0ea9b097b977255857a4e193da101f96f1
Lakshiga <lakshiga20021216@gmail.com>
2025-12-19 22:14:25 +0800

    Backend done

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,26 +1,7 @@
-import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-  }
-  if (networkError) {
-    console.log(`[Network error]: ${networkError}`);
-  }
-});
-
-const link = from([
-  errorLink,
-  new HttpLink({ uri: 'http://localhost:4000/graphql' }),
-]);
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  link,
+  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 });
 
